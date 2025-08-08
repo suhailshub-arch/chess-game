d64f9fdd9a578331ad108ce89813ae431d03edd0
Suhail Sallahudin <suhail.sallahudin@gmail.com>
2025-08-08 11:58:29 +0100

    Restructured project to seperate the client anad server

--- chess-server/src/main/java/com/server/dto/JoinMessageDTO.java
+++
@@ -1,6 +0,0 @@
-package com.server.dto;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record JoinMessageDTO(String playerId, String name, int rating) {}
